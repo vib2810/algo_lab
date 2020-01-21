b356d01f6e0331e0de51cbbff4b3e6e908f002ee
vib2810 <vib2810@gmail.com>
2020-01-21 16:52:25 +0530

    assignment 3

--- a1_1.cpp
+++ a1_1.cpp
@@ -22,16 +22,17 @@ double hirec(int n)
 }  
 hilo hilorec(int n)
 {
-    double hirec=1, lorec=0;
-    for(int i=0; i<n; i++)
+    if(n==0)
     {
-        double hirec_old=hirec;
-        hirec=2*hirec+lorec;
-        lorec=hirec_old+lorec;
+        hilo temp;
+        temp.hirec=1;
+        temp.lorec=0;
+        return temp;   
     }
+    hilo temp_m1=hilorec(n-1);
     hilo temp;
-    temp.hirec=hirec;
-    temp.lorec=lorec;
+    temp.hirec=2*temp_m1.hirec+temp_m1.lorec;
+    temp.lorec= temp_m1.hirec+temp_m1.lorec;
     return temp;
 }
 hilo hiloformula(int n)
@@ -49,13 +50,13 @@ int main()
     cout<<"Enter n: ";
     cin>>n;
 
-    cout << "Method 1 n=" << n << " hirec: "<< hirec(n) << " lorec: " <<lorec(n)<<endl;
+    cout << "Method 0 n=" << n << " hirec: "<< hirec(n) << " lorec: " <<lorec(n)<<endl;
 
     hilo ans2=hilorec(n);
-    cout << "Method 2 n=" << n << " hirec: "<< ans2.hirec << " lorec: " <<ans2.lorec<<endl;   
+    cout << "Method 1 n=" << n << " hirec: "<< ans2.hirec << " lorec: " <<ans2.lorec<<endl;   
 
     hilo ans3=hiloformula(n);
-    cout << "Method 3 n=" << n << " hirec: "<< ans3.hirec << " lorec: " <<ans3.lorec<<endl;   
+    cout << "Method 2 n=" << n << " hirec: "<< ans3.hirec << " lorec: " <<ans3.lorec<<endl;   
 
     return 0; 
 } 
